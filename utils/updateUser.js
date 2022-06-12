1753474144bc71fb88afd94207827e20642db849
VladimirParmon <parmon_vladimir@bk.ru>
2022-06-12 22:03:11 +0300

    add put method handler

---
+++ utils/updateUser.js
@@ -0,0 +1,36 @@
+import { getBodyData } from "./getBodyData.js";
+import { store } from "../store.js";
+import { validate as uuidValidate } from 'uuid';
+
+export function updateUser(req, res) {
+  const userID = req.url.split('/')[3];
+  if(!uuidValidate(userID)) {
+    res.writeHead(400, 'Not a valid ID', { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify('Not a valid ID'));
+    return;
+  }
+  
+  const indexOfUserToBeChanged = store.findIndex((userData) => userData.id === userID);
+  if (indexOfUserToBeChanged !== -1) {
+    changeUser(indexOfUserToBeChanged);
+  } else {
+    res.writeHead(404, 'User does not exist', { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify('User does not exist'));
+  }
+
+  async function changeUser(indexInStore) {
+    try {
+      const bodyData = await getBodyData(req)
+      const body = JSON.parse(bodyData);
+
+      for(let prop in body) {
+        store[indexInStore][prop] = body[prop];
+      }
+
+      res.writeHead(200, 'User updated', { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify('User updated'));
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
